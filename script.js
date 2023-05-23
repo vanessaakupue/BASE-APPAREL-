0e92dc268ea10889f7947ed4374a9ddc900881c8
vanessaakupue <107567601+vanessaakupue@users.noreply.github.com>
2023-05-23 16:48:47 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const submit = document.querySelector(".submit");
+// const error = document.querySelector(".errorIcon");
+const errorMsg = document.querySelector(".errorMessage");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const label = document.querySelector("label");
+
+form.addEventListener("submit", validate);
+submit.addEventListener("click", validate);
+
+function validate(e) {
+    e.preventDefault();
+
+    const inputValue = input.value.trim();
+
+    if (inputValue === "") {
+        errorMsg.style.display = "block";
+        label.style.display = "block";
+
+    } else if (!isEmail(inputValue)) {
+        errorMsg.style.display = "block";
+        label.style.display = "block";
+
+    } else {
+        errorMsg.style.display = "none";
+        label.style.display = "none";
+    }
+}
+
+function isEmail(input) {
+    return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(input)
+
+}
+
+
+
